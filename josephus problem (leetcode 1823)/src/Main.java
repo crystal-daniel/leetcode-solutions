e0b054588db2dab27c807cd295c7f956e4b32042
Ranjith Crystal <ranjith05chris@gmail.com>
2023-08-19 12:41:01 +0530

    leetcode problem 1823 added

---
+++ josephus problem (leetcode 1823)/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	static int calculate(int n,int k)
+    {
+        if(n==1)
+            return 0;
+        return (calculate(n-1,k)+k)%n;
+    }
+    public static int findTheWinner(int n, int k) {
+        int ans = calculate(n,k);
+        return ans+1;
+    }
+	public static void main(String args[])
+	{
+		
+		int result=findTheWinner(5,2);
+		System.out.println(result);
+	}
+}
