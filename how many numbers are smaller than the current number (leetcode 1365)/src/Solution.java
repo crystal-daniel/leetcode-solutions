4de7fe6a5a559f8a054c8e6a8dc4df1ff62e81e0
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-06 20:54:51 +0530

    leetcode problem 1365 added

---
+++ how many numbers are smaller than the current number (leetcode 1365)/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Solution {
+	public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int [] ans = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(i!=j && nums[j]<nums[i])
+                {
+                    ans[i]++;
+                }
+            }
+        }
+        return ans;
+    }
+	public static void main(String [] args)
+	{
+		int [] nums = {8,1,2,2,3};
+		System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+	}
+}
