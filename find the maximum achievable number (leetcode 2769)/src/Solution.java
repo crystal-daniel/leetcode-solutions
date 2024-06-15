c7a39a32fa3a050397f1125deec7465469dd564c
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-15 21:25:56 +0530

    leetcode problem 2769 added

---
+++ find the maximum achievable number (leetcode 2769)/src/Solution.java
@@ -0,0 +1,9 @@
+
+public class Solution {
+	public static int theMaximumAchievableX(int num, int t) {
+        return num+2*t;
+    }
+	public static void main(String[] args) {
+		System.out.println(theMaximumAchievableX(3,2));
+	}
+}
