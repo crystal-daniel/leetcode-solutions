3a5e1bbf454b6ec5708609fe6b7c08e3ac855c9e
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-17 20:56:18 +0530

    leetcode problem 2235 added

---
+++ add two integers (leetcode 2235)/src/Solution.java
@@ -0,0 +1,9 @@
+
+public class Solution {
+	public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+	public static void main(String[] args) {
+		System.out.println(sum(5,4));
+	}
+}
