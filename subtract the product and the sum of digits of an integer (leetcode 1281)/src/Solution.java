11fad5d396d470ee51904212a5a90fb9e9814f39
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-19 21:28:52 +0530

    leetcode problem 1281 added

---
+++ subtract the product and the sum of digits of an integer (leetcode 1281)/src/Solution.java
@@ -0,0 +1,17 @@
+
+public class Solution {
+	public static int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+        while(n!=0)
+        {
+            sum+=(n%10);
+            product*=(n%10);
+            n/=10;
+        }
+        return product-sum;
+    }
+	public static void main(String[] args) {
+		System.out.println(subtractProductAndSum(234));
+	}
+}
