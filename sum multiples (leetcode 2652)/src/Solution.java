95514554e1c0e280e440935e7ad8ed2ca2b3d498
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-20 21:36:33 +0530

    leetcode problem 2652 added

---
+++ sum multiples (leetcode 2652)/src/Solution.java
@@ -0,0 +1,18 @@
+
+public class Solution {
+	public static int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 || i%5==0 || i%7==0)
+            {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+	public static void main(String[] args) {
+		System.out.println(sumOfMultiples(7));
+
+	}
+}
