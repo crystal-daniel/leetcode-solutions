ada8b24d45be73687aaf1f1cb0045bec47f908f2
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-24 21:07:14 +0530

    leetcode problem 1688 added

---
+++ count of matches in tournament (leetcode 1688)/src/Solution.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+	public static int numberOfMatches(int n) {
+        int totalMatches = 0;
+        while(n>1)
+        {
+            if(n%2==0)
+            {
+                totalMatches = totalMatches + (n/2);
+                n = n/2;
+            }
+            else
+            {
+                totalMatches = totalMatches + (n-1)/2;
+                n = ((n-1)/2) + 1;
+            }
+        }
+        return totalMatches;
+    }
+	public static void main(String[] args) {
+		System.out.println(numberOfMatches(14));
+	}
+}
