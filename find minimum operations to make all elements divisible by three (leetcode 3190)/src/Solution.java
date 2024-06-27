e21a0b88e2be7762cd88629b714773a4b51968fc
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-27 20:43:14 +0530

    leetcode problem 3190 added

---
+++ find minimum operations to make all elements divisible by three (leetcode 3190)/src/Solution.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+	public static int minimumOperations(int[] nums) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%3!=0)
+                count++;
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+		int [] nums = {1,2,3,4};
+		System.out.println(minimumOperations(nums));
+	}
+}
