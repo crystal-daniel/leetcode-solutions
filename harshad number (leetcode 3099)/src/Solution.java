678a35231da1a4de9d3c8f410b2d3606e1856ab7
Ranjith Crystal <ranjith05chris@gmail.com>
2024-06-30 20:34:23 +0530

    leetcode problem 3099 added

--- harshad number (leetcode 3099)/src/Solution.java
+++ harshad number (leetcode 3099)/src/Solution.java
@@ -1,9 +1,20 @@
 
 public class Solution {
-
+	public static int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0, temp = x;
+        while(x!=0)
+        {
+            char a = (char) (x % 10);
+            sum += a;
+            x /= 10;
+        }
+        if(temp%sum==0)
+        {
+            return sum;
+        }
+        return -1;
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(sumOfTheDigitsOfHarshadNumber(18));
 	}
-
 }
