cfc00c5145df1ab133ac0db3e0ddbaa0c98a1617
Ranjith Crystal <ranjith05chris@gmail.com>
2024-07-07 21:38:41 +0530

    leetcode problem 1252 added

---
+++ cells with odd values in a matrix (leetcode 1252)/src/Solution.java
@@ -0,0 +1,43 @@
+
+public class Solution {
+	public static int oddCells(int m, int n, int[][] indices) {
+        int [][] ans = new int[m][n];
+        int odd = 0;
+        for(int i=0;i<indices.length;i++)
+        {
+            int j = 0;
+            while(j<n)
+            {
+                ans[indices[i][0]][j]++;
+                if(ans[indices[i][0]][j]%2!=0)
+                {
+                    odd++;
+                }
+                else
+                {
+                    odd--;
+                }
+                j++;
+            }
+            j = 0;
+            while(j<m)
+            {
+                ans[j][indices[i][1]]++;
+                if(ans[j][indices[i][1]]%2!=0)
+                {
+                    odd++;
+                }
+                else
+                {
+                    odd--;
+                }
+                j++;
+            }
+        }
+        return odd;
+    }
+	public static void main(String[] args) {
+		int m = 2, n = 3, indices[][] = {{0,1},{1,1}};
+		System.out.println(oddCells(m, n, indices));
+	}
+}
