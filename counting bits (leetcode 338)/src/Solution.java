449ed3224a3ddaebf2475c709a303fc6cca1f45c
Ranjith Crystal <ranjith05chris@gmail.com>
2024-07-18 23:25:25 +0530

    leetcode problem 338 added

---
+++ counting bits (leetcode 338)/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Solution {
+	public static int[] countBits(int n) {
+        int [] res = new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            res[i] = countingBits(i);
+        }
+        return res;
+    }
+    static int countingBits(int n) {
+        int count = 0;
+        while(n!=0)
+        {
+            if(n%2==1)
+            {
+                count++;
+            }
+            n /= 2;
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(countBits(5)));
+	}
+}
