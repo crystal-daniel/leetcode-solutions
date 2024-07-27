c5316cb5fda56e4c1e2f69236fd5f9e2df250a99
Ranjith Crystal <ranjith05chris@gmail.com>
2024-07-27 21:41:18 +0530

    leetcode problem 832 added

---
+++ flipping an image (leetcode 832)/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Solution {
+	public static int[][] flipAndInvertImage(int[][] image) {
+        for(int i=0;i<image.length;i++)
+        {
+            int l = 0;
+            int h = image.length - 1;
+            while(l<h)
+            {
+                image[i][l] = image[i][l] ^ image[i][h];
+                image[i][h] = image[i][l] ^ image[i][h];
+                image[i][l] = image[i][l] ^ image[i][h];
+                l++;
+                h--;
+            }
+            for(int j=0;j<image.length;j++)
+            {
+                image[i][j] = image[i][j] ^ 1;
+            }
+        }
+        return image;
+    }
+	public static void main(String[] args) {
+		int [][] image = {{1,1,0},{1,0,1},{0,0,0}};
+		flipAndInvertImage(image);
+		for(int [] n: image)
+		{
+			System.out.print(Arrays.toString(n));
+		}
+	}
+}
