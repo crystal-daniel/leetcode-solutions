b6f8dfb6f296e858e5e69654d26b5127fae39533
Ranjith Crystal <ranjith05chris@gmail.com>
2025-02-03 14:42:10 +0530

    leetcode problem -1 added

---
+++ number of subsequences that is equal to the sum (leetcode -1)/src/Solution.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Solution {
+	List<List<Integer>> answer = new ArrayList<>();
+	int res2 = 0;
+	
+	//using a data structure
+    void calculateSubseq(int index, int [] nums, int target, int sum, List<Integer> res)
+    {
+        if(index>=nums.length)
+        {
+            if(sum==target)
+            {
+                answer.add(new ArrayList<>(res));
+            }
+            return;
+        }
+        res.add(nums[index]);
+        calculateSubseq(index+1, nums, target, sum+nums[index], res);
+        res.remove(res.size()-1);
+        calculateSubseq(index+1, nums, target, sum-nums[index], res);
+    }
+    public int numSubseq(int[] nums, int target) {
+        List<Integer> res = new ArrayList<>();
+        calculateSubseq(0, nums, target, 0, res);
+        return answer.size();
+    }
+    
+    //without using data structure
+	void calculateSubseq2(int index, int [] nums, int target, int sum)
+    {
+        if(index>=nums.length)
+        {
+            if(sum==target)
+            {
+                res2++;
+            }
+            return;
+        }
+        calculateSubseq2(index+1, nums, target, sum+nums[index]);
+        calculateSubseq2(index+1, nums, target, sum-nums[index]);
+    }
+    public int numSubseq2(int[] nums, int target) {
+        calculateSubseq2(0, nums, target, 0);
+        return res2;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        int target = 5;
+        Solution obj = new Solution();
+        System.out.println("Using Data Structure: "+obj.numSubseq(nums, target));
+        System.out.println("Without using Data Structure: "+obj.numSubseq2(nums, target));
+    }
+}
